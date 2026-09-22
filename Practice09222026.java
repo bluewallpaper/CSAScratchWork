65aacae4ad063ed0255ee3874e7b4e7388ffefd3
printHelloWorld1000 <luca.drsampaio@gmail.com>
2026-09-22 12:38:11 -0700

    Create Practice09222026.java

---
+++ Practice09222026.java
@@ -0,0 +1,17 @@
+public class Practice09222026 {
+   public static void main(String[] args)
+{
+      System.out.print("Marry ");
+      marysPet("dog", ", ");
+      marysPet("dog", ". ");
+      
+   }
+   public static void marysPet(String petName, String punctuation) { // method definition or method declaration
+                        // method signature: littlelamb()
+                        // method header: public static void littlelamb()
+                        // parameters: littlelamb(parameters-here)
+                        // argument: System.out.print("argument-here")
+      System.out.print(petName + punctuation);
+                        // method call: print("little lamb ")
+   }
+}
