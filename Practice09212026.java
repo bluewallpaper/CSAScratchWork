2bfc5e4584fd825d64959d0effcb1e81f6046f34
printHelloWorld1000 <luca.drsampaio@gmail.com>
2026-09-22 12:13:17 -0700

    Create Practice09212026.java

---
+++ Practice09212026.java
@@ -0,0 +1,11 @@
+public class Practice09212026 
+{
+   public static void main(String[] args) {
+      int myNum =  1;
+      // myNum = myNum + 1; 
+      // myNum++; ----> myNum = myNum + 1
+      myNum += 1;
+      
+      System.out.println("myNum is equal to " + myNum);
+   }
+}
